6ac70f0adfc9c6cec6efef820891a58baafa9a22
Ahmad Bin Masoodd <ahmabinmasoodd@gmail.com>
2026-02-02 23:48:29 +0500

    Add app theming structure with utils, constants, and widget themes

---
+++ lib/utils/Theme/theme.dart
@@ -0,0 +1,46 @@
+import 'package:e_commerce/utils/Theme/widgets_theme/appbar_theme.dart';
+import 'package:e_commerce/utils/Theme/widgets_theme/bottom_sheet_theme.dart';
+import 'package:e_commerce/utils/Theme/widgets_theme/checkbox_theme.dart';
+import 'package:e_commerce/utils/Theme/widgets_theme/chip_theme.dart';
+import 'package:e_commerce/utils/Theme/widgets_theme/elevated_button_theme.dart';
+import 'package:e_commerce/utils/Theme/widgets_theme/outlined_button_theme.dart';
+import 'package:e_commerce/utils/Theme/widgets_theme/text_field_theme.dart';
+import 'package:e_commerce/utils/Theme/widgets_theme/text_theme.dart';
+import 'package:e_commerce/utils/constants/colors.dart';
+import 'package:flutter/material.dart';
+
+class UAppTheme {
+  UAppTheme._();
+  static ThemeData lightTheme = ThemeData(
+    useMaterial3: true,
+    brightness: Brightness.light,
+    fontFamily: '',
+    primaryColor: UColors.primary,
+    disabledColor: UColors.grey,
+    textTheme: UTextTheme.lightTextTheme,
+    scaffoldBackgroundColor: UColors.white,
+    chipTheme: UChipTheme.lightChipTheme,
+    appBarTheme: UAppBarTheme.lightAppBarTheme,
+    checkboxTheme: UCheckboxTheme.lightCheckboxTheme,
+    elevatedButtonTheme: UElevatedButtonTheme.lightElevatedButtonTheme,
+    outlinedButtonTheme: UOutlinedButtonTheme.lightOutlinedButtonTheme,
+    bottomSheetTheme: UBottomSheetTheme.lightBottomSheetTheme,
+    inputDecorationTheme: UTextFormFieldTheme.lightInputDecorationTheme
+  );
+  static ThemeData darkTheme = ThemeData(
+    useMaterial3: true,
+    fontFamily: '',
+    brightness: Brightness.dark,
+    textTheme: UTextTheme.darkTextTheme,
+    primaryColor: UColors.primary,
+    disabledColor: UColors.grey,
+    scaffoldBackgroundColor: UColors.black,
+      chipTheme: UChipTheme.darkChipTheme,
+    appBarTheme: UAppTheme.darkTheme,
+    checkboxTheme: UCheckboxTheme.darkCheckboxTheme,
+    elevatedButtonTheme: UElevatedButtonTheme.darkElevatedButtonTheme,
+    outlinedButtonTheme: UOutlinedButtonTheme.darkOutlinedButtonTheme,
+    bottomSheetTheme: UBottomSheetTheme.darkBottomSheetTheme,
+    inputDecorationTheme: UTextFormFieldTheme.darkInputDecorationTheme
+  );
+}
---
+++ lib/utils/constants/colors.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+class UColors{
+  UColors._();
+
+  static const primary=Color(0xff0857A0);
+
+  //text color
+  static const Color textPrimary=Color(0xff333333);
+  static const Color textSecondary=Color(0xFF6c7570);
+  static const Color textWhite=Colors.white;
+
+  //Background color
+  static const  Color light=Color(0xFFF6F6F6);
+  static const  Color dark=Color(0xFF272727);
+
+  //Button Colors
+  static const  Color buttonPrimary=Color(0xFF0857A0);
+  static const  Color buttonDisabled=Color(0xFFC4C4C4);
+
+  //Border colors
+  static const  Color borderPrimary=Color(0xFFD9D9D9);
+  static const  Color borderSecondary=Color(0xFFE6E6E6);
+
+  //Errors and validation colors
+  static const  Color error=Color(0xFFD32F2F);
+  static const  Color success=Color(0xFF388E2C);
+  static const Color warning=Color(0xFFF57C00);
+  static const  Color info=Color(0xFF1976D2);
+
+  //Neural Shades
+  static const  Color black=Color(0xFF232323);
+  static const  Color darkerGrey=Color(0xFF4F4F4F);
+  static const  Color darkGrey=Color(0xFFE0E0E0);
+  static const  Color grey=Color(0xFFE0E0E0);
+  static const  Color lightGrey=Color(0xFFF9F9F9);
+  static const  Color white=Color(0xFFFFFFFF);
+
+}
