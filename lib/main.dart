ae49a03b0b02c6b67c0ea7fde9f689af63051d4c
Ahmad Bin Masoodd <ahmabinmasoodd@gmail.com>
2026-02-16 01:10:59 +0500

    Configure Firebase and ignore secrets

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,17 @@
 import 'package:e_commerce/my_app.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 
+import 'firebase_options.dart';
 
-void main() {
+
+void main() async{
   //final widgetBinding=WidgetsFlutterBinding.ensureInitialized();
   //FlutterNativeSplash.preserve(widgetsBinding: widgetBinding);
-
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]);
   runApp(const MyApp());
 }
